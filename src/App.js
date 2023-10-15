df4714915c2928f883e5aaac8c62ec2dbb835a5b
Savan <sawanpatel2508@gmail.com>
2023-10-15 15:17:50 -0400

    Add routes in the application

--- src/App.js
+++ src/App.js
@@ -1,39 +1,13 @@
+import { Routes, Route } from "react-router-dom";
 import "./categories.styles.scss";
 import "./components/catergory-items/CategoryItem";
-import { Directory } from "./components/directory/Directory";
+import { Home } from "./routes/Home";
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "women's",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "men's",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
   return (
-    <div>
-      <Directory categories={categories} />
-    </div>
+    <Routes>
+      <Route path={"/"} element={<Home />} />
+    </Routes>
   );
 };
 
---
+++ src/routes/Home.jsx
@@ -0,0 +1,36 @@
+import { Directory } from "../components/directory/Directory";
+
+export const Home = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    },
+    {
+      id: 2,
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    },
+    {
+      id: 3,
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    },
+    {
+      id: 4,
+      title: "women's",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    },
+    {
+      id: 5,
+      title: "men's",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    },
+  ];
+  return (
+    <div>
+      <Directory categories={categories} />
+    </div>
+  );
+};
