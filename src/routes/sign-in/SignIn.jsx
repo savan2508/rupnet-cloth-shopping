0a5126fadc52ca6299b892629fd3abd0c71ebc2d
Savan <sawanpatel2508@gmail.com>
2023-10-21 15:06:20 -0400

    Add google redirect method for log in

--- src/routes/sign-in/SignIn.jsx
+++ src/routes/sign-in/SignIn.jsx
@@ -1,7 +1,15 @@
-import { signInWithGooglePopup} from "../../utils/firebase/firebase.utils";
-import {createUserDocumentFromAuth} from "../../utils/firebase/firebase.utils";
+import { useEffect } from "react";
+import { getRedirectResult } from 'firebase/auth'
+import { auth, signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect} from "../../utils/firebase/firebase.utils";
 
 export const SignIn = () => {
+
+    useEffect(async () => {
+        const response = await getRedirectResult(auth);
+        if(response) {
+            const userDocRef = await createUserDocumentFromAuth(response.user);
+        }
+    }, []);
 
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
@@ -13,6 +21,9 @@ export const SignIn = () => {
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>
                 Sign in with Google
+            </button>
+            <button onClick={signInWithGoogleRedirect}>
+                Sign in with Google Redirect
             </button>
         </div>
     )
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -30,6 +30,7 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, provider);
 
 export const db = getFirestore();
 
